c251df64e840a7a8bb7eb66687a03cb4472bfe3c
Resul Takak <resultakak@gmail.com>
2019-03-16 00:01:07 +0300

    Setup Package

---
+++ src/__tests__/Greeter.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("../index");
+test('My Greeter', function () {
+    expect(index_1.Greeter('Carl')).toBe('Hello Carl');
+});
